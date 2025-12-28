528cf4095b55e09670c2a644d68f685847ebbc86
Anit paul <94783299+Anitpaul@users.noreply.github.com>
2025-12-28 21:11:46 +0530

    Done

---
+++ Chapter3/problem3.py
@@ -0,0 +1,3 @@
+a = input("Enter your sentence:\n").strip()
+
+print(f"The sentence has double space: {a.find("  ")}") 
---
+++ Chapter3/problem4.py
@@ -0,0 +1,3 @@
+a = input("Enter your sentence, darling:\n").strip()
+
+print(f"The corrected sentence\n{a.replace("  ", " ")}")
