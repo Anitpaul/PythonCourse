d5f64143c25a5d186e00765a46ce29f5eaa395ab
Anit paul <94783299+Anitpaul@users.noreply.github.com>
2025-12-30 17:20:29 +0530

    Done

---
+++ Chapter5/problem1.py
@@ -0,0 +1,12 @@
+dict1 = {
+    "Chair": "Kursi",
+    "Ice": "Baraf",
+    "Wrong": "Galat",
+    "Right": "Sahi",
+    "Soul": "Atma",
+    "Fear": "Dur"
+}
+
+a = input("Enter the hindi word: ").strip().capitalize()
+
+print(f"The english Word is {dict1[a]}")
---
+++ Chapter5/problem2.py
@@ -0,0 +1,7 @@
+s = set()
+
+for i in range(0,8):
+    temp = int(input("Enter the number: "))
+    s.add(temp)
+
+print("The numbers are", s)
